68673fb291036261378e60e7646532fe082fe397
wcolts2000 <wcolts2000@gmail.com>
2018-11-02 22:03:46 -0700

    Updates

---
+++ precache-manifest.6d7c20a031ca0eee0dd485661cc1d002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ca15db6880073249c7",
+    "url": "/javascript_snake-/static/js/runtime~main.b0ca15db.js"
+  },
+  {
+    "revision": "86b9659a607cdbee7c01",
+    "url": "/javascript_snake-/static/js/main.86b9659a.chunk.js"
+  },
+  {
+    "revision": "84727026245d8c05e875",
+    "url": "/javascript_snake-/static/js/1.84727026.chunk.js"
+  },
+  {
+    "revision": "86b9659a607cdbee7c01",
+    "url": "/javascript_snake-/static/css/main.86b7cc1e.chunk.css"
+  },
+  {
+    "revision": "d1045bd157fc82e16d9c6b337ae02ff7",
+    "url": "/javascript_snake-/index.html"
+  }
+];
